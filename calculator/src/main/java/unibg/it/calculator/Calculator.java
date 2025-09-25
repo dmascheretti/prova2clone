8b2fd40b79c449c31248e9dcb0b508531001ade9
dmascheretti <d.mascheretti1@studenti.unibg.it>
2025-09-25 16:38:19 +0200

    vsvsdsv

---
+++ calculator/src/main/java/unibg/it/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package unibg.it.calculator;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Calculator {
+	
+	
+
+	public static void main( String[] args )
+    {
+		System.out.print(true);
+		
+    }
+}

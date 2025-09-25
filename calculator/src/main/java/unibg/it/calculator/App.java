b3ea2156a657826b3e24ab98e308387345f83390
dmascheretti <d.mascheretti1@studenti.unibg.it>
2025-09-25 16:49:48 +0200

    no

--- calculator/src/main/java/unibg/it/calculator/App.java
+++ calculator/src/main/java/unibg/it/calculator/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Salnitri bel mandrillone");
+        System.out.println();
+        System.out.println();
+        
     }
 }
